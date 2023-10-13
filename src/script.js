0ea567af47e5cfed43f76c9d274691e04ea0fe5b
jsc723 <ja235711@gmail.com>
2023-10-12 22:44:11 -0700

    working

--- src/script.js
+++ src/script.js
@@ -24,36 +24,21 @@ function calculateLinkStrength() {
 }
 
 // Create sample data
-const nodes = [{ id: 1, x: centerX, y: centerY, cardData: {name: 'a', level: 1} },
-{ id: 2, x: centerX+100, y: centerY+100,cardData: {name: 'a', level: 2} },
-{ id: 3, x: centerX-100, y: centerY-100, cardData: {name: 'a', level: 3} }];
+const nodes = [];
 
 
-const links = [{ source: nodes[0], target: nodes[1] }, { source: nodes[1], target: nodes[2] }];
+const links = [];
 
-console.log(nodes, links)
 var link = svg.selectAll('.link')
-    .data(links)
-    .enter()
-    .append('line')
-    .attr('class', 'link')
-    .attr('x1', d => d.source.x)
-    .attr('y1', d => d.source.y)
-    .attr('x2', d => d.target.x)
-    .attr('y2', d => d.target.y);
+    .data(links);
 
 var node = svg.selectAll('.node')
-    .data(nodes)
-    .enter()
-    .append('circle')
-    .attr('class', 'node')
-    .attr('r', 20)
-    .attr('cx', d => d.x)
-    .attr('cy', d => d.y)
-    .call(d3.drag()
-        .on('start', dragStarted)
-        .on('drag', dragging)
-        .on('end', dragEnded));
+    .data(nodes);
+
+// Create a selection for the text elements
+var textElements = svg.selectAll('.node-label')
+    .data(nodes);
+
 
 const linkForce = d3.forceLink().strength(calculateLinkStrength()).id((d) => d.id);
 
@@ -79,6 +64,11 @@ function ticked() {
     node
         .attr('cx', (d) => d.x)
         .attr('cy', (d) => d.y);
+
+    
+    textElements
+        .attr('x', (d) => d.x)
+        .attr('y', (d) => d.y)
 }
 
 function dragStarted(event, d) {
@@ -122,68 +112,28 @@ function loadData() {
 loadData();
 
 
+function hasEdge(card1, card2) {
+    let x = 0
+    if (card1.Attack == card2.Attack) {
+        x += 1
+    }
+    if (card1.Defense == card2.Defense) {
+        x += 1
+    }
+    if (card1.Attribute == card2.Attribute) {
+        x += 1
+    }
+    if (card1.Level == card2.Level) {
+        x += 1
+    }
+    if (card1.Type == card2.Type) {
+        x += 1
+    }
+    return x === 1
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Select the button element
-    const addButton = document.getElementById('add-node-button');
-    console.log(addButton)
-    // Event listener for adding a new node and connecting it to node with id=2
-    addButton.addEventListener('click', () => {
-        // Calculate the center (average position) of all nodes
-        const center = nodes.reduce(
-            (accumulator, node) => {
-                accumulator.x += node.x;
-                accumulator.y += node.y;
-                return accumulator;
-            },
-            { x: 0, y: 0 }
-        );
-
-        center.x /= nodes.length;
-        center.y /= nodes.length;
-        // Generate a new node with a unique ID
-        const newNode = { id: nodes.length + 1, x: center.x, y: center.y, cardData: {name: 'b', level: nodes.length+1}  };
-
-        // Push the new node to the nodes array
-        nodes.push(newNode);
-
-        const selectedNodes = nodes.filter(node => node.cardData.level % 2 === 0);
-        console.log(selectedNodes)
-        selectedNodes.forEach(node => {
-            console.log('node: ',  node)
-            const newEdge = { source: newNode, target: node };
-            links.push(newEdge);
-            const newEdgeElement = svg
-                .append('line')
-                    .attr('class', 'link')
-                    .attr('x1', newNode.x)
-                    .attr('y1', newNode.y)
-                    .attr('x2', node.x)
-                    .attr('y2', node.y);
-        })
-
-        // Append a new circle for the new node
-        const newNodeElement = svg
-        .append('circle')
-            .attr('class', 'node')
-            .attr('r', 20)
-            .attr('cx', newNode.x)
-            .attr('cy', newNode.y)
-            .call(d3.drag()
-                .on('start', dragStarted)
-                .on('drag', dragging)
-                .on('end', dragEnded));
-
-        link = svg.selectAll('.link').data(links)
-        node = svg.selectAll('.node').data(nodes)
-
-        linkForce.strength(calculateLinkStrength());
-
-        // Update the simulation to consider the new node and edge
-        simulation.nodes(nodes);
-        simulation.force('link').links(links);
-        simulation.alpha(1).restart();
-    });
 
     document.getElementById('search-button').addEventListener('click', performSearch);
 
@@ -212,8 +162,75 @@ document.addEventListener('DOMContentLoaded', function () {
     
         // Do something with the associated object
         console.log('Clicked result data:', objectData);
+
+        // Calculate the center (average position) of all nodes
+        const center = nodes.reduce(
+            (accumulator, node) => {
+                accumulator.x += node.x;
+                accumulator.y += node.y;
+                return accumulator;
+            },
+            { x: 0, y: 0 }
+        );
+
+        center.x = center.x / nodes.length + 5*Math.random();
+        center.y = center.y / nodes.length + 5*Math.random();
+        // Generate a new node with a unique ID
+        const newNode = { id: nodes.length + 1, x: center.x, y: center.y, cardData: objectData  };
+
+        // Push the new node to the nodes array
+        nodes.push(newNode);
+
+        const selectedNodes = nodes.filter(node => hasEdge(node.cardData, newNode.cardData));
+        console.log(selectedNodes)
+        selectedNodes.forEach(node => {
+            console.log('node: ',  node)
+            const newEdge = { source: newNode, target: node };
+            links.push(newEdge);
+            const newEdgeElement = svg
+                .append('line')
+                    .attr('class', 'link')
+                    .attr('x1', newNode.x)
+                    .attr('y1', newNode.y)
+                    .attr('x2', node.x)
+                    .attr('y2', node.y);
+        })
+
+        // Add text elements for new nodes
+        const textElem = svg
+        .append('text')
+        .attr('class', 'node-label') // Add a class to style the text
+        .attr('x', newNode.x) // Set the x-coordinate based on your node data
+        .attr('y', newNode.y) // Set the y-coordinate based on your node data
+        .text(newNode.cardData.Name) // Set the text content to the node's name
+        .attr('text-anchor', 'middle') // Center the text on the node
+        .attr('dy', '0.3em'); // Adjust the vertical position if needed
+
+        // Append a new circle for the new node
+        const newNodeElement = svg
+        .append('circle')
+            .attr('class', 'node')
+            .attr('r', 20)
+            .attr('cx', newNode.x)
+            .attr('cy', newNode.y)
+            .call(d3.drag()
+                .on('start', dragStarted)
+                .on('drag', dragging)
+                .on('end', dragEnded));
+
+        link = svg.selectAll('.link').data(links)
+        node = svg.selectAll('.node').data(nodes)
+        textElements = svg.selectAll('.node-label').data(nodes)
+
+        linkForce.strength(calculateLinkStrength());
+
+        // Update the simulation to consider the new node and edge
+        simulation.nodes(nodes);
+        simulation.force('link').links(links);
+        simulation.alpha(1).restart();
+
     }
-//ニードル
+
     function displaySearchResults(results) {
         const resultsDiv = document.getElementById('search-results');
         resultsDiv.innerHTML = ''; // Clear previous results
