ead861df7e9174f894494f9d0d44227dd7a6913f
jsc723 <ja235711@gmail.com>
2023-10-13 15:21:57 -0700

    finish export and import

--- src/script.js
+++ src/script.js
@@ -52,6 +52,15 @@ const simulation = d3.forceSimulation()
     .force('link', linkForce)
     .on('tick', ticked);
 
+function restartSimulation(){
+    linkForce.strength(calculateLinkStrength());
+
+    // Update the simulation to consider the new node and edge
+    simulation.nodes(nodes);
+    simulation.force('link').links(links);
+    simulation.alpha(1).restart();
+}
+
 
 // Add nodes and edges to the simulation
 simulation.nodes(nodes);
@@ -61,7 +70,7 @@ function ticked() {
 
     nodes.forEach(node => {
         node.x = Math.max(0, Math.min(svgWidth, node.x));
-        node.y = Math.max(0, Math.min(svgHeight-10, node.y));
+        node.y = Math.max(0, Math.min(svgHeight - 10, node.y));
     });
 
     link
@@ -74,7 +83,7 @@ function ticked() {
         .attr('cx', (d) => d.x)
         .attr('cy', (d) => d.y);
 
-    
+
     textElements
         .attr('x', (d) => d.x)
         .attr('y', (d) => d.y)
@@ -144,8 +153,14 @@ function hasEdge(card1, card2) {
 
 document.addEventListener('DOMContentLoaded', function () {
 
+    document.getElementById('clear-btn').addEventListener('click', () => clearGraph());
+    document.getElementById('import-btn').addEventListener('click', () => importJSON());
+    document.getElementById('export-btn').addEventListener('click', () => exportJSON());
+
+
+
     // Add an event listener to the input field for the "keydown" event
-    document.getElementById('search-input').addEventListener('keydown', function(event) {
+    document.getElementById('search-input').addEventListener('keydown', function (event) {
         if (event.key === 'Enter') {
             // Check if the pressed key is "Enter"
             event.preventDefault(); // Prevent the default behavior (form submission)
@@ -177,7 +192,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function handleButtonAddClick(event) {
         // Retrieve the associated object from the data attribute
         const objectData = JSON.parse(event.currentTarget.getAttribute('data-object'));
-    
+
         // Do something with the associated object
         console.log('Clicked result data:', objectData);
         if (nodes.find(node => node.cardData.Name === objectData.Name)) {
@@ -194,10 +209,10 @@ document.addEventListener('DOMContentLoaded', function () {
             { x: 0, y: 0 }
         );
 
-        center.x = center.x / nodes.length + 5*Math.random();
-        center.y = center.y / nodes.length + 5*Math.random();
+        center.x = center.x / nodes.length + 5 * Math.random();
+        center.y = center.y / nodes.length + 5 * Math.random();
         // Generate a new node with a unique ID
-        const newNode = { id: getNewId(), x: center.x, y: center.y, cardData: objectData  };
+        const newNode = { id: getNewId(), x: center.x, y: center.y, cardData: objectData };
 
         // Push the new node to the nodes array
         nodes.push(newNode);
@@ -205,31 +220,31 @@ document.addEventListener('DOMContentLoaded', function () {
         const selectedNodes = nodes.filter(node => hasEdge(node.cardData, newNode.cardData));
         console.log(selectedNodes)
         selectedNodes.forEach(node => {
-            console.log('node: ',  node)
+            console.log('node: ', node)
             const newEdge = { source: newNode, target: node };
             links.push(newEdge);
             const newEdgeElement = svg
                 .append('line')
-                    .attr('class', 'link')
-                    .attr('x1', newNode.x)
-                    .attr('y1', newNode.y)
-                    .attr('x2', node.x)
-                    .attr('y2', node.y);
+                .attr('class', 'link')
+                .attr('x1', newNode.x)
+                .attr('y1', newNode.y)
+                .attr('x2', node.x)
+                .attr('y2', node.y);
         })
 
         // Add text elements for new nodes
         const textElem = svg
-        .append('text')
-        .attr('class', 'node-label') // Add a class to style the text
-        .attr('x', newNode.x) // Set the x-coordinate based on your node data
-        .attr('y', newNode.y) // Set the y-coordinate based on your node data
-        .text(newNode.cardData.Name) // Set the text content to the node's name
-        .attr('text-anchor', 'middle') // Center the text on the node
-        .attr('dy', '0.3em'); // Adjust the vertical position if needed
+            .append('text')
+            .attr('class', 'node-label') // Add a class to style the text
+            .attr('x', newNode.x) // Set the x-coordinate based on your node data
+            .attr('y', newNode.y) // Set the y-coordinate based on your node data
+            .text(newNode.cardData.Name) // Set the text content to the node's name
+            .attr('text-anchor', 'middle') // Center the text on the node
+            .attr('dy', '0.3em'); // Adjust the vertical position if needed
 
         // Append a new circle for the new node
         const newNodeElement = svg
-        .append('circle')
+            .append('circle')
             .attr('class', 'node')
             .attr('r', 20)
             .attr('cx', newNode.x)
@@ -243,19 +258,14 @@ document.addEventListener('DOMContentLoaded', function () {
         node = svg.selectAll('.node').data(nodes)
         textElements = svg.selectAll('.node-label').data(nodes)
 
-        linkForce.strength(calculateLinkStrength());
-
-        // Update the simulation to consider the new node and edge
-        simulation.nodes(nodes);
-        simulation.force('link').links(links);
-        simulation.alpha(1).restart();
+        restartSimulation();
 
     }
 
     function handleButtonDeleteClick(event) {
         // Retrieve the associated object from the data attribute
         const objectData = JSON.parse(event.currentTarget.getAttribute('data-object'));
-    
+
         // Do something with the associated object
         console.log('Clicked result data:', objectData);
         const toRemove = nodes.find(node => node.cardData.Name === objectData.Name)
@@ -281,12 +291,7 @@ document.addEventListener('DOMContentLoaded', function () {
         node = svg.selectAll('.node').data(nodes)
         textElements = svg.selectAll('.node-label').data(nodes)
 
-        linkForce.strength(calculateLinkStrength());
-
-        // Update the simulation to consider the new node and edge
-        simulation.nodes(nodes);
-        simulation.force('link').links(links);
-        simulation.alpha(1).restart();
+        restartSimulation();
 
     }
 
@@ -310,11 +315,126 @@ document.addEventListener('DOMContentLoaded', function () {
                 buttonDeleteElement.textContent = 'Delete';
                 buttonDeleteElement.setAttribute('data-object', JSON.stringify(result));
                 buttonDeleteElement.addEventListener('click', handleButtonDeleteClick);
-    
+
                 resultElement.appendChild(buttonAddElement);
                 resultElement.appendChild(buttonDeleteElement);
                 resultsDiv.appendChild(resultElement);
             });
+        }
+    }
+
+    function clearGraph() {
+        svg.selectAll('.link').remove()
+        svg.selectAll('.node').remove()
+        svg.selectAll('.node-label').remove()
+        node = svg.selectAll('.node')
+        link = svg.selectAll('.link')
+        textElements = svg.selectAll('.node-label')
+
+        nodes = []
+        links = []
+
+        restartSimulation();
+    }
+
+    // Function to download JSON data as a file
+    function exportJSON() {
+        // Create a JSON object (or use an existing one)
+        const jsonData = {
+            nodes: nodes,
+            links: links.map((link) => {
+                return {
+                    source: link.source.id, 
+                    target: link.target.id
+                }
+            })
+        };
+
+        // Convert the JSON object to a string
+        const jsonString = JSON.stringify(jsonData, null, 2);
+
+        // Create a Blob (Binary Large Object) from the JSON string
+        const blob = new Blob([jsonString], { type: 'application/json' });
+
+        // Create a URL for the Blob
+        const url = URL.createObjectURL(blob);
+
+        // Create a temporary link element for downloading
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'data.json'; // Specify the filename for the downloaded file
+        a.style.display = 'none';
+
+        // Append the link to the document and trigger a click event to initiate the download
+        document.body.appendChild(a);
+        a.click();
+
+        // Clean up: remove the temporary link and revoke the Blob URL
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+    }
+
+    function importJSON() {
+        const fileInput = document.getElementById('fileInput');
+        console.log('import json ', fileInput)
+
+        // Check if a file is selected
+        if (fileInput.files.length > 0) {
+            clearGraph();
+            console.log('hello')
+            const selectedFile = fileInput.files[0];
+            const reader = new FileReader();
+
+            // Set up a callback function for when the file is loaded
+            reader.onload = function(event) {
+                console.log(event.target.result);
+
+                let data = JSON.parse(event.target.result);
+                nodes = data.nodes;
+                links = data.links.map(link => {
+                    return {
+                        source: nodes.find(n => n.id === link.source),
+                        target: nodes.find(n => n.id === link.target)
+                    }
+                });
+                console.log(nodes, links)
+                link = svg.selectAll('.link')
+                    .data(links)
+                    .enter()
+                    .append('line')
+                    .attr('class', 'link')
+                    .attr('x1', d => d.source.x)
+                    .attr('y1', d => d.source.y)
+                    .attr('x2', d => d.target.x)
+                    .attr('y2', d => d.target.y);
+                
+                textElements = svg.selectAll('.node-label')
+                    .data(nodes)
+                    .enter()
+                    .append('text')
+                    .attr('class', 'node-label') // Add a class to style the text
+                    .attr('x', d => d.x) // Set the x-coordinate based on your node data
+                    .attr('y', d => d.y) // Set the y-coordinate based on your node data
+                    .text(d => d.cardData.Name) // Set the text content to the node's name
+                    .attr('text-anchor', 'middle') // Center the text on the node
+                    .attr('dy', '0.3em'); // Adjust the vertical position if needed
+
+                node = svg.selectAll('.node')
+                    .data(nodes)
+                    .enter()
+                    .append('circle')
+                    .attr('class', 'node')
+                    .attr('r', 20)
+                    .attr('cx', d => d.x)
+                    .attr('cy', d => d.y)
+                    .call(d3.drag()
+                        .on('start', dragStarted)
+                        .on('drag', dragging)
+                        .on('end', dragEnded));
+
+                restartSimulation();
+            };
+            reader.readAsText(selectedFile);
         }
     }
 
